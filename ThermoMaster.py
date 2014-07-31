d143ba0238dee460c66ce59a205bad4037c512fc
zivraf <zivraf@hotmail.com>
2014-07-31 09:22:22 -0700

    b

--- ThermoMaster.py
+++ ThermoMaster.py
@@ -13,6 +13,11 @@ GPIO.setup (7, GPIO.OUT )
 GPIO.setup (11, GPIO.OUT)
 
 LED_STAT = False
+LED_PIN = 11
+BUTTON_PIN = 15
+SENSOR_PIN = 7
+
+AMQP_CONN_STR = "amqps://owner:BFHk1n+EmRpTpbMtIH53zwcYxEpcDke/DLv1MOeKa1w=@thermomaster.servicebus.windows.net/statusupdt"
 
 def blinker (times):
     index = 0
@@ -27,6 +32,29 @@ def blinker (times):
     if LED_STAT == True:
         GPIO.output(11,True)
 
+def buttonCallBack (channel):
+    print "--callback"
+    if LED_STAT:
+        print "--turn off"
+        LED_STAT = False
+    else:
+        print "--turn on"
+        LED_STAT = True    
+    GPIO.output (LED_PIN,LED_STAT)
+
+def sendMessage (oper, value):
+    message = Message()
+    message.address = AMQP_CONN_STR
+    message.body = u"ThermoMaster 1.0 - Status Update"
+    prop = {}
+    prop [u"DeviceId"] = u"28-000005658920"
+    currTime =  time.asctime( time.localtime(time.time()) )
+    prop [u"UpdtTime"] = currTime
+    prop [u"cmd"] = oper
+    prop [u"value"] = value
+    message.properties = prop
+    messenger.put(message)
+    messenger.send()
 
 def main():
     print "Welcome To ThermoMaster demo"
@@ -39,6 +67,7 @@ def main():
         
     blinker(2)
 
+    GPIO.add_event_detect (BUTTON_PIN, GPIO.RISING, callback= buttonCallBack, councetime=500)
     print "start"
     messenger = Messenger()
     message = Message()
@@ -46,43 +75,48 @@ def main():
 
     datafile = open ("tempreading.log","w")
 
-    print "--reading temprature"
+    while True:
 
-    GPIO.output (7, GPIO.HIGH)
-    tfile = open ("/sys/bus/w1/devices/28-000005658920/w1_slave")
-    text = tfile.read()
-    tfile.close()
-    print "-- closed dev file"
-    secondline = text.split ("\n")[1]
-    tempData = secondline.split(" ")[9]
-    temprature = float (tempData[2:])
-    temprature = temprature / 1000
-    print "-- temprature is " , temprature
-    datafile.write(str(temprature)+ "\n")
+        print "--reading temprature"
+
+        GPIO.output (7, GPIO.HIGH)
+        tfile = open ("/sys/bus/w1/devices/28-000005658920/w1_slave")
+        text = tfile.read()
+        tfile.close()
+        print "-- closed dev file"
+        secondline = text.split ("\n")[1]
+        tempData = secondline.split(" ")[9]
+        temprature = float (tempData[2:])
+        temprature = temprature / 1000
+        print "-- temprature is " , temprature
+        datafile.write(str(temprature)+ "\n")
 
 
-    message.body = u"ThermoMaster 1.0 - Status Update"
-    prop = {}
-    prop [u"DeviceId"] = u"28-000005658920"
-    prop [u"Temprature"] = temprature
+        message.body = u"ThermoMaster 1.0 - Status Update"
+        prop = {}
+        prop [u"DeviceId"] = u"28-000005658920"
+        currTime =  time.asctime( time.localtime(time.time()) )
+        prop [u"UpdtTime"] = currTime
+        prop [u"Temprature"] = temprature
 
-    message.properties = prop
-    #message.subject = u"ThermoMaster Status Update"
-    #currTime = strftime(u"%Y-%m-%d %H:%M:%S", gmtime()
-    #message.properties [u"UpdateTime"] = currTime
-    #message.properties [u"DeviceId"] = "28-000005658920"
-    #message.properties [u"Temprature"] = temprature
-    
-    print "-- Message properties ", message.properties
+        message.properties = prop
+        #message.subject = u"ThermoMaster Status Update"
+        #message.properties [u"UpdateTime"] = currTime
+        #message.properties [u"DeviceId"] = "28-000005658920"
+        #message.properties [u"Temprature"] = temprature
+        
+        print "-- Message properties ", message.properties
 
-    messenger.put(message)
-    messenger.send()
+        messenger.put(message)
+        messenger.send()
 
-    print "-- Message sent"
+        print "-- Message sent"
 
- 
-    time.sleep (1)
-    GPIO.output (7, GPIO.LOW)
+     
+        time.sleep (1)
+        GPIO.output (7, GPIO.LOW)
+
+        time.sleep (10)
    
     datafile.close()
     print "--Leaving Main"
